4a33253875dc5921161cadaac74039bd9d5ad8e9
chazzy1 <chazzy1@gmail.com>
2017-05-18 22:34:48 +0900

    working on relation margin

--- annotation/static/js/annotation_relation_tool.js
+++ annotation/static/js/annotation_relation_tool.js
@@ -111,7 +111,7 @@ var $J1 = (function (module){
         if (_p.currentTypeSystemMode == "L" && entityType.logical_value) {
             entityTypeLabel = entityType.logical_value;
         };
-
+console.log(tokenEle)
         var tokenEntityTypeMarker = $('<div class="tokenEntityTypeMarker">'+entityTypeLabel+'</div>');
         tokenEntityTypeMarker.css("background-color",entityType.sireProp.backGroundColor);
         tokenEntityTypeMarker.css("color",entityType.sireProp.color);
@@ -119,7 +119,9 @@ var $J1 = (function (module){
         sentenceEle.append(tokenEntityTypeMarker);
         tokenEntityTypeMarker.css("left",tokenEle.position().left);
         tokenEntityTypeMarker.css("top",tokenEle.position().top-30);
-        tokenEntityTypeMarker.css("width",tokenEle.width());
+        //줄바뀜 있을때 버그가 있음
+        //tokenEntityTypeMarker.css("width",tokenEle.width());
+        tokenEntityTypeMarker.css("width",tokenEle.find(".gtcTokenText").width());
 
         tokenEntityTypeMarker.attr("mentionId",tokenEle.attr("mentionId"));
         tokenEntityTypeMarker.attr("entityTypeLabel",tokenEle.attr("entityTypeLabel"));
@@ -131,9 +133,9 @@ var $J1 = (function (module){
         //일단 한개 넣어놔보고, 이전것과 높이가 같으면 필요없구나...하고 다시 제거하는 로직으로 간다.
 
         //그래야 나중에 높이 변할때 한개만 바꿔도 실제 간격이 변하게 할 수 있다.
-
+        //일단 일정한 간격으로 무조건 늘려준다.
         var newEle = $('<span class="gtcTokenRelationMargin"></span>');
-        tokenEle.append(newEle);
+        tokenEle.find(".gtcTokenText").append(newEle);
         var newEleTop = newEle.offset().top;
         sentenceEle.find(".gtcTokenRelationMargin").each(function(index,ele){
             var isNewMarginNeeded = true;
@@ -148,6 +150,8 @@ var $J1 = (function (module){
     };
 
     function recalcRelationAreaPadding(sentenceEle){
+
+    //사용안됨
         //같은줄에 relationLabel 이 몇개나 있는지 보고 이에따라 gtcTokenRelationMargin의 높이를 조절해 준다.
         sentenceEle.find(".gtcTokenRelationMargin").each(function(index,marginEle){
             var marginBotton = $(marginEle).offset().top + $(marginEle).height();
@@ -168,8 +172,6 @@ var $J1 = (function (module){
 
             });
 
-
-
         });
 
 
@@ -177,6 +179,37 @@ var $J1 = (function (module){
 
     }
 
+    function recalcRelationAreaPadding1(lineMarginGroupInfo){
+        if (lineMarginGroupInfo.length > 0){
+        //console.log(lineMarginGroupInfo)
+        }
+        //{"paddingEleContainer":gtcToken , "relationLabels":[]};
+
+        for (var k in lineMarginGroupInfo){
+            var marginGroup = lineMarginGroupInfo[k];
+            //console.log(marginGroup)
+
+            var marginEle = marginGroup.paddingEle;
+
+            //console.log(marginEle.offset().top);
+            //console.log(marginGroup.relationLabels[0].offset().top)
+
+            for (var i in marginGroup.relationLabels){
+                var relationLabel = marginGroup.relationLabels[i];
+                if (marginEle.offset().top > relationLabel.offset().top+10){
+                    var newMargin = marginEle.height() + (marginEle.offset().top - relationLabel.offset().top);
+//엉뚱한 대상을 늘리는 버그가 있는듯.
+                    marginEle.css("height",newMargin);
+                    activateRelationToolDocumentUI();
+                    break;
+
+                }
+            }
+
+        }
+
+
+    }
 
     function drawRelationTypeItem(item){
         var style = "border-color:" + item.sireProp.backGroundColor + "; color:"+item.sireProp.color;
@@ -195,9 +228,13 @@ var $J1 = (function (module){
         $("#list-relation-type").append(itemEle);
     };
 
-
-
     _p.resetRelationDisplay = function(){
+        $("#document-holder").find(".gtcSentence").each(function(index,ele){
+            _p.resetSentenceRelationDisplay($(ele));
+        });
+    }
+
+    _p.resetSentenceRelationDisplay = function(sentenceEle){
 
 
         //var relations = _p.loadedGroundTruth.relations;
@@ -205,11 +242,22 @@ var $J1 = (function (module){
         var lastMarkerTop = null;
         //console.log(relations)
 
+        var lineIndex = 0;
+        var lineMarginGroupInfo = {};
+        var marginContainer = null;
         //선 그리기 문제때문에 relation 순서대로 하면 안된다.
         //mention이 포착되는 순서대로 그에따른 relation 을 처리해 줘야한다.
-        $("#document-holder").find(".tokenEntityTypeMarker").each(function(index,ele){
+        var marginEle = null;
+        sentenceEle.find(".tokenEntityTypeMarker").each(function(index,ele){
             ele = $(ele);
             var mentionId = ele.attr("mentionId");
+
+            var marginContainer = sentenceEle.find('span[mentionId="'+mentionId+'"]');
+            if (marginContainer.find(".gtcTokenRelationMargin").length > 0){
+                marginEle = $(marginContainer.find(".gtcTokenRelationMargin")[0]);
+            }
+
+
             //우선 tokenEntityTypeMarker 하나를 고른다음, 그놈에게 속한 relation들을 가져와서,
             var relations = $.grep(_p.loadedGroundTruth.relations, function(e){ return e.args[0] == mentionId; });
             //relation 을 그린다.
@@ -222,7 +270,7 @@ var $J1 = (function (module){
                 var parentMarkerEle = $($('div[mentionId="'+parentMarkerId+'"]')[0]);
 
                 var childMarkerEle = $($('div[mentionId="'+childMarkerId+'"]')[0]);
-                var sentenceEle = $(parentMarkerEle.closest(".gtcSentence"));
+                //var sentenceEle = $(parentMarkerEle.closest(".gtcSentence"));
                 var relationType = _p.loadedRelationPropLabelMap[relation.type];
 
                 var label = relation.type;
@@ -250,6 +298,19 @@ var $J1 = (function (module){
                 } else {
                     //다른줄이 시작됐다면.
                     relationLabelStackCount = 1;
+                    lineIndex++;
+                };
+
+                console.log(lineMarginGroupInfo)
+
+                if (lineMarginGroupInfo[lineIndex]){
+                    lineMarginGroupInfo[lineIndex].relationLabels.push(relationLabelEle);
+                } else {
+
+                    var gtcToken = sentenceEle.find('span[mentionId="'+parentMarkerId+'"]');
+
+                    lineMarginGroupInfo[lineIndex] = {"paddingEle":marginEle , "relationLabels":[]};
+                    lineMarginGroupInfo[lineIndex].relationLabels.push(relationLabelEle);
                 };
 
 
@@ -299,14 +360,12 @@ var $J1 = (function (module){
                 });
 
 
-                recalcRelationAreaPadding(sentenceEle);
-
-
-
             };
 
 
+
         });
+        recalcRelationAreaPadding1(lineMarginGroupInfo);
 
 
     };
